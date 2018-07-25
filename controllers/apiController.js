3d8e486a8bf576bd6916074f537f7b7e55e3f2ad
Vivek Singh <vivek.kumar@fluper.com>
2018-07-25 18:33:00 +0530

    Liniting added

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,169 +1,170 @@
 /** @module apiController */
 
-var jwt = require('jsonwebtoken');
-const jsonpatch = require('jsonpatch');
-var fs = require('fs');
-const http = require("http");
-const https = require("https");
-const URL = require('url');
-const sharp = require('sharp');
-const path = require('path');
+var jwt = require("jsonwebtoken")
+const jsonpatch = require("jsonpatch")
+var fs = require("fs")
+const http = require("http")
+const https = require("https")
+const URL = require("url")
+const sharp = require("sharp")
+const path = require("path")
 
-const SIZE = 50; // Default size for the image resize
+const SIZE = 50 // Default size for the image resize
 
 module.exports = {
-    /** Generates JWT Token for the user signing using APP_KEY */
-    login: (req, res) => {
-        if (!req.body.username) {
-            res.status(400).json({
-                'message': "Username missing"
-            })
-            return
-        }
-        if (!req.body.password) {
-            res.status(400).json({
-                'message': "Password missing"
-            })
-            return
-        }
+	/** Generates JWT Token for the user signing using APP_KEY */
+	login: (req, res) => {
+		if (!req.body.username) {
+			res.status(400).json({
+				"message": "Username missing"
+			})
+			return
+		}
+		if (!req.body.password) {
+			res.status(400).json({
+				"message": "Password missing"
+			})
+			return
+		}
 
-        let username = req.body.username
+		let username = req.body.username
 
-        var token = jwt.sign({
-            name: username
-        }, process.env.APP_KEY, {
-            expiresIn: '1440m'
-        });
+		var token = jwt.sign({
+			name: username
+		}, process.env.APP_KEY, {
+			expiresIn: "1440m"
+		})
 
-        res.json({
-            success: true,
-            message: 'Successful',
-            token: token
-        });
-        return
-    },
+		res.json({
+			success: true,
+			message: "Successful",
+			token: token
+		})
+		return
+	},
 
-    image: (req, res) => {
-        let file = path.join(__dirname, '../tmp/' + req.params.image);
-        if (!fs.existsSync(file)) {
-            res.json({
-                message: 'File not found',
-            });
-            return
-        }
-        res.download(file);
-        return
-    },
+	image: (req, res) => {
+		let file = path.join(__dirname, "../tmp/" + req.params.image)
+		if (!fs.existsSync(file)) {
+			res.json({
+				message: "File not found",
+			})
+			return
+		}
+		res.download(file)
+		return
+	},
 
-    /** Generates thumbnail for the given url of public image*/
-    thumbnail: (req, res) => {
+	/** Generates thumbnail for the given url of public image*/
+	thumbnail: (req, res) => {
 
-        var dir = './tmp';
+		var dir = "./tmp"
 
-        if (!fs.existsSync(dir)){
-            fs.mkdirSync(dir);
-        }
+		if (!fs.existsSync(dir)){
+			fs.mkdirSync(dir)
+		}
 
-        if (!req.body.url) {
-            res.status(400).json({
-                'message': "url missing"
-            })
-            return
-        }
-        const url = req.body.url
+		if (!req.body.url) {
+			res.status(400).json({
+				"message": "url missing"
+			})
+			return
+		}
+		const url = req.body.url
 
-        //Regex for valid url
-        const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
+		//Regex for valid url
+		const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
 
-        if (!urlRegex.test(url)) {
-            res.json({
-                message: 'Invalid url',
-            });
-            return
-        }
+		if (!urlRegex.test(url)) {
+			res.json({
+				message: "Invalid url",
+			})
+			return
+		}
 
-        let host = URL.parse(url)
+		let host = URL.parse(url)
 
-        if (host.protocol == null) {
-            res.json({
-                message: 'Invalid url',
-            });
-            return
-        }
+		if (host.protocol == null) {
+			res.json({
+				message: "Invalid url",
+			})
+			return
+		}
 
-        protocol = host.protocol.slice(0, -1)
+		var protocol = host.protocol.slice(0, -1)
 
-        var file = fs.createWriteStream('tmp/code.jpg');
+		var file = fs.createWriteStream("tmp/code.jpg")
+		var client = null;
+		(protocol == "https") ? client = https: client = http
 
-        (protocol == "https") ? client = https: client = http
+		client.get(url, function (response) {
 
-        client.get(url, function (response) {
+			// Regex to check for the image mime type
+			const headerRegex = /(image\/).*/
 
-            // Regex to check for the image mime type
-            const headerRegex = /(image\/).*/
+			if (response.statusCode != 200 || !headerRegex.test(response.headers["content-type"])) {
+				res.json({
+					message: "Image not found on the given url",
+				})
+				return
+			}
 
-            if (response.statusCode != 200 || !headerRegex.test(response.headers['content-type'])) {
-                res.json({
-                    message: 'Image not found on the given url',
-                });
-                return
-            }
+			response.on("data", function (data) {
+				file.write(data)
+			}).on("end", function () {
+				file.end()
+				sharp("tmp/code.jpg")
+					.resize(SIZE, SIZE)
+					.ignoreAspectRatio()
+					.toBuffer()
+					.then(data => {
+						const fileName = Math.floor(Math.random() * 100000000) + 1
+						fs.createWriteStream(`tmp/${fileName}.jpg`).write(data)
+						res.status(200).json({
+							resizedImage: process.env.APP_URL + "/api/image/" + fileName + ".jpg"
+						})
+						return
+					})
+					.catch(err => {
+						res.json({
+							message: "Error in resizing",
+							error: err
+						})
+						return
+					})
+				return
+			})
+		})
+	},
 
-            response.on('data', function (data) {
-                file.write(data);
-            }).on('end', function () {
-                file.end();
-                sharp('tmp/code.jpg')
-                    .resize(SIZE, SIZE)
-                    .ignoreAspectRatio()
-                    .toBuffer()
-                    .then(data => {
-                        const fileName = Math.floor(Math.random() * 100000000) + 1;
-                        fs.createWriteStream(`tmp/${fileName}.jpg`).write(data);
-                        res.status(200).json({
-                            resizedImage: process.env.APP_URL + '/api/image/' + fileName + '.jpg'
-                        });
-                        return
-                    })
-                    .catch(err => {
-                        res.json({
-                            message: 'Error in resizing',
-                            error: err
-                        });
-                        return
-                    });
-                return
-            });
-        });
-    },
-
-    /** Applying json patch */
-    applyJsonPatch: (req, res) => {
-        if (!req.body.obj) {
-            res.status(400).json({
-                'message': "Object is missing"
-            })
-            return
-        }
-        if (!req.body.patch) {
-            res.status(400).json({
-                'message': "Patch obj is missing"
-            })
-            return
-        }
-        let obj = req.body.obj
-        let patch = req.body.patch
-        try {
-            patcheddoc = jsonpatch.apply_patch(obj, [patch]);
-        } catch (error) {
-            res.status(400).json({
-                'message': "Not a valid patch object"
-            });
-            return
-        }
-        res.status(200).json({
-            'new_json': patcheddoc
-        })
-        return
-    }
+	/** Applying json patch */
+	applyJsonPatch: (req, res) => {
+		if (!req.body.obj) {
+			res.status(400).json({
+				"message": "Object is missing"
+			})
+			return
+		}
+		if (!req.body.patch) {
+			res.status(400).json({
+				"message": "Patch obj is missing"
+			})
+			return
+		}
+		let obj = req.body.obj
+		let patch = req.body.patch
+		var patcheddoc = null
+		try {
+			patcheddoc = jsonpatch.apply_patch(obj, [patch])
+		} catch (error) {
+			res.status(400).json({
+				"message": "Not a valid patch object"
+			})
+			return
+		}
+		res.status(200).json({
+			"new_json": patcheddoc
+		})
+		return
+	}
 }
--- server.js
+++ server.js
@@ -1,50 +1,50 @@
-'use strict';
+"use strict"
 
-const express = require('express');
-require('dotenv').config();
+const express = require("express")
+require("dotenv").config()
 
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const apiRoutes = require('./routes/apiRoutes')
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const apiRoutes = require("./routes/apiRoutes")
 
-var fs = require('fs')
-var path = require('path')
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
-    flags: 'a'
+var fs = require("fs")
+var path = require("path")
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+	flags: "a"
 })
 
 
-const PORT = process.env.PORT || 9090;
-const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 9090
+const HOST = process.env.HOST || "0.0.0.0"
 
-const app = express();
+const app = express()
 
-app.set('appKey', process.env.APP_KEY);
+app.set("appKey", process.env.APP_KEY)
 
-app.use(morgan('combined', {
-    stream: accessLogStream
+app.use(morgan("combined", {
+	stream: accessLogStream
 }))
 
 app.use(bodyParser.urlencoded({
-    extended: false
-}));
+	extended: false
+}))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use (function (error, req, res, next){
-    if(error){
-        res.status(400).send({'msg': "Invalid json"});
-        return
-    }
-});
+	if(error){
+		res.status(400).send({"msg": "Invalid json"})
+		return
+	}
+})
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes)
 
-app.all('*', function (req, res) {
-    res.status(400).send("Page not found");
-    return
-});
+app.all("*", function (req, res) {
+	res.status(400).send("Page not found")
+	return
+})
 
 
 app.listen(PORT, HOST, () => {
-    console.log(`Running on http://${HOST}:${PORT}`);
-});
+	console.log(`Running on http://${HOST}:${PORT}`)
+})
